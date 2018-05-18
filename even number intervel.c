e180f5b7cf67d393b0f4a5a8428466bdb142b3b8
Anbunaveen <35570975+Anbunaveen@users.noreply.github.com>
2018-05-18 02:18:46 -0700

    Update even number intervel.c

--- even number intervel.c
+++ even number intervel.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-#include<math.h>
 
 int main()
 {
-    int base,power,result;
-    printf("ENTER THE BASE AND POWER\n");
-    scanf("%d %d",&base,&power);
-    result=pow(base,power);
-    printf("result=%d",result);
+    int first,last,i;
+    printf("ENTER THE first AND last number\n");
+    scanf("%d %d",&first,&last);
+    for(i=first;i<last;i++)
+    {
+        if(i%2==0)
+       { 
+           printf("%d\n",i);
+       }
+    }
      return 0;
 }
+
